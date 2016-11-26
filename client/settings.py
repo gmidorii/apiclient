63fb5b037d8e6b5e6ac665df590aa7e3564c4c3e
tagasouichiro <stitow14@gmail.com>
2016-11-26 18:57:18 +0900

    add CRUD

--- client/settings.py
+++ client/settings.py
@@ -123,6 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-SATICFILES_DIR = (
+STATICFILES_DIRS = (
 	os.path.join(BASE_DIR, "static"),
 )
--- cms/urls.py
+++ cms/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import url
 from cms import views
 
 urlpatterns = [
-	url(r'^domain/$', views.domain_list, name='domain_list')
-	url(r'^domain/add/$', views.domain_edit, name='domain_add')
-	url(r'^domain/mod/(?P<domain_id>\d+)/$', views.domain_edit, name='domain_mod')
-	url(r'^domain/del/(?P<domain_id>\d+)/$', views.domain_del, name='domain_del')
+	url(r'^domain/$', views.domain_list, name='domain_list'),
+	url(r'^domain/add/$', views.domain_edit, name='domain_add'),
+	url(r'^domain/mod/(?P<domain_id>\d+)/$', views.domain_edit, name='domain_mod'),
+	url(r'^domain/del/(?P<domain_id>\d+)/$', views.domain_del, name='domain_del'),
 ]
